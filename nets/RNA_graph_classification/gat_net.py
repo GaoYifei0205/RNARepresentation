29896e08a5c5bed409b5075976bbfe6635f5d3fc
VickyBonbon <1403549373@qq.com>
2021-10-31 21:46:15 +0800

    revise class GAT

--- nets/RNA_graph_classification/gat_net.py
+++ nets/RNA_graph_classification/gat_net.py
@@ -5,16 +5,23 @@ from layers.gat_layer import GraphAttentionLayer, SpGraphAttentionLayer
 
 
 class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+    #def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+    def __init__(self, net_params):
         """Dense version of GAT."""
         super(GAT, self).__init__()
-        self.dropout = dropout
+        nfeat = net_params['nfeat']
+        nhid = net_params['nhid']
+        nclass = net_params['nclass']
+        alpha = net_params['alpha']
+        nheads = net_params['nheads']
 
-        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
+        self.dropout = net_params['dropout']
+
+        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=net_params['dropout'], alpha=alpha, concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
             self.add_module('attention_{}'.format(i), attention)
 
-        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
+        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=net_params['dropout'], alpha=alpha, concat=False)
 
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
@@ -25,14 +32,21 @@ class GAT(nn.Module):
 
 
 class SpGAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+    def __init__(self, net_params):
         """Sparse version of GAT."""
         super(SpGAT, self).__init__()
-        self.dropout = dropout
+        nfeat = net_params['nfeat']
+        nhid = net_params['nhid']
+        nclass = net_params['nclass']
+        alpha = net_params['alpha']
+        nheads = net_params['nheads']
+
+        self.dropout = net_params['dropout']
+
 
         self.attentions = [SpGraphAttentionLayer(nfeat,
                                                  nhid,
-                                                 dropout=dropout,
+                                                 dropout=net_params['dropout'],
                                                  alpha=alpha,
                                                  concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
@@ -40,7 +54,7 @@ class SpGAT(nn.Module):
 
         self.out_att = SpGraphAttentionLayer(nhid * nheads,
                                              nclass,
-                                             dropout=dropout,
+                                             dropout=net_params['dropout'],
                                              alpha=alpha,
                                              concat=False)
 
