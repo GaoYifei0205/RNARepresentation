c7707f872bc252e99135a892696f58804150d179
VickyBonbon <1403549373@qq.com>
2021-10-31 21:35:41 +0800

    ADD GAT

--- nets/RNA_graph_classification/load_net.py
+++ nets/RNA_graph_classification/load_net.py
@@ -4,20 +4,22 @@
 """
 from nets.RNA_graph_classification.gcn_net import GCNNet
 from nets.RNA_graph_classification.graphsage_net import GraphSageNet
-
+from nets.RNA_graph_classification.gat_net import GAT
 
 def GCN(net_params):
     return GCNNet(net_params)
 
-
 def GraphSage(net_params):
     return GraphSageNet(net_params)
 
+def GAT(net_params):
+    return GAT(net_params)
 
 def gnn_model(MODEL_NAME, net_params):
     models = {
         'GCN': GCN,
         'GraphSage': GraphSage,
+        'GAT':GAT
     }
         
     return models[MODEL_NAME](net_params)
